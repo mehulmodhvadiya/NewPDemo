c50a651dc9b07f433318b70101945c9cd97b9cae
mehul modhvadiya <modhvadiya.mehul@gmail.com>
2020-08-17 19:37:55 +0530

    call article api and make design

---
+++ NewPDemo/Model/ArticleModel.swift
@@ -0,0 +1,9 @@
+//
+//  ArticleModel.swift
+//  NewPDemo
+//
+//  Created by mehul modhvadiya on 17/08/20.
+//  Copyright © 2020 mehul modhvadiya. All rights reserved.
+//
+
+import Foundation
---
+++ NewPDemo/View/ArticleCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleCell.swift
+//  NewPDemo
+//
+//  Created by mehul modhvadiya on 17/08/20.
+//  Copyright © 2020 mehul modhvadiya. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
